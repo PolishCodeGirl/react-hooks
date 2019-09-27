d1fb44e5a7051a6e0194f33c7f5c4a479be71a0f
Marzenka <marzenka@Marzenas-MacBook-Pro.local>
2019-09-27 09:03:43 +0200

    Setup project

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render() { 
+        return (
+            <div className="book-list">
+                <ul>
+                    <li>Feel the fear and do it antway</li>
+                    <li>The third door</li>
+                    <li>Slight edge</li>
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default BookList;
