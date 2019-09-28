99f992a78f407b7e502fda31b84b297fcf45274c
Marzenka <marzenka@Marzenas-MacBook-Pro.local>
2019-09-28 08:55:56 +0200

    Add context to the project

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
+    static contextType = ThemeContext;
+    
     render() { 
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+
         return (
-            <div className="book-list">
+            <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
                 <ul>
-                    <li>Feel the fear and do it antway</li>
-                    <li>The third door</li>
-                    <li>Slight edge</li>
+                    <li style={{background: theme.ui}}>Feel the fear and do it antway</li>
+                    <li style={{background: theme.ui}}>The third door</li>
+                    <li style={{background: theme.ui}}>Slight edge</li>
                 </ul>
             </div>
         );
