84480667671ed5d40da51ae7f8a47fe2571bff4a
PolishCodeGirl <malachowska.marzena@gmail.com>
2019-10-09 09:01:55 +0200

    Add books component using hooks

---
+++ src/components/BookForm.js
@@ -0,0 +1,26 @@
+import React, { useState, useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
+
+const BookForm = () => {
+    const { addBook } = useContext(BookContext);
+
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        addBook(title, author);
+        setTitle('');
+        setAuthor('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="title" value={title} onChange={event => setTitle(event.target.value)} />
+            <input type="text" placeholder="author" value={author} onChange={event => setAuthor(event.target.value)} />
+            <input type="submit" value="Add book"/>
+        </form>
+    );
+}
+ 
+export default BookForm;
