4588c014958fbaf3f1fb37f0a6af8836ebeef920
PolishCodeGirl <malachowska.marzena@gmail.com>
2019-10-11 08:54:51 +0200

    Store books list in localeStorage

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 
 import { bookReducer } from '../reducers/bookReducer';
 
@@ -8,7 +8,14 @@ const BookContextProvider = props => {
     const [books, dispatch] = useReducer(bookReducer, [
         {title: '21 Lessons for the 21st Century', author: 'Yuval Noah Harari', id: 1},
         {title: 'How to Win Friends and Influence People', author: 'Dale Carnegie', id: 2},
-    ]);
+    ], () => {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books))
+    }, [books]);
 
     return (
         <BookContext.Provider value={{books, dispatch}}>
