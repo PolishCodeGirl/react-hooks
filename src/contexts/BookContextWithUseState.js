f0c202cc4747fbf5bd7b37fae6eb6d674fc823ac
PolishCodeGirl <malachowska.marzena@gmail.com>
2019-10-11 08:37:42 +0200

    useReducer usage

--- src/contexts/BookContextWithUseState.js
+++ src/contexts/BookContextWithUseState.js
@@ -1,17 +1,24 @@
-import React, { createContext, useReducer } from 'react';
-
-import { bookReducer } from '../reducers/bookReducer';
+import React, { createContext, useState } from 'react';
+import uuid from 'uuid/v1';
 
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-    const [books, dispatch] = useReducer(bookReducer, [
+    const [books, setBooks] = useState([
         {title: '21 Lessons for the 21st Century', author: 'Yuval Noah Harari', id: 1},
         {title: 'How to Win Friends and Influence People', author: 'Dale Carnegie', id: 2},
     ]);
 
+    const addBook = (title, author) => {
+        setBooks([...books, { title, author, id: uuid()}]);
+    }
+
+    const removeBook = id => {
+        setBooks(books.filter(book => book.id !== id))
+    }
+
     return (
-        <BookContext.Provider value={{books, dispatch}}>
+        <BookContext.Provider value={{books, addBook, removeBook}}>
             { props.children }
         </BookContext.Provider>
     );
